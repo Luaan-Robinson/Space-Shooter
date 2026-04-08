e882e58a50a69de89d8e553455a6ed65a7e9d906
Luaan-Robinson <106935625+Luaan-Robinson@users.noreply.github.com>
2026-04-08 15:43:49 +0200

    added player movement using arrow keys

--- code/main.py
+++ code/main.py
@@ -20,9 +20,9 @@ x = 100
 
 # image imports
 player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha() # creates a dynamic filepath to the spaceship image
-player_rect = player_surf.get_frect(topleft = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)) # creates rect and places center of rect at (0,0)
-player_direction = pygame.math.Vector2(2, -1)
-player_speed = 10
+player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)) # creates rect and places center of rect at (0,0)
+player_direction = pygame.math.Vector2()
+player_speed = 300
 
 star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
 star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)] 
@@ -35,11 +35,29 @@ laser_rect = laser_surf.get_frect(bottomleft = (20, WINDOW_HEIGHT-20))
 
 # event loop that checks for events such as key presses, mouse clicks, etc. and handles them accordingly
 while running:
-    clock.tick(10) # frame rate
+    dt = clock.tick() / 1000 # frame rate
+    
+    # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False 
+           
+       # if event.type == pygame.MOUSEMOTION:
+       #     print(event.pos) 
+       #     player_rect.center = event.pos   
 
+    # input
+    # print(pygame.mouse.get_rel())
+    keys = pygame.key.get_pressed()
+    player_direction.x = int(keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) # sets player direction to 1 if right key is pressed, -1 if left key is pressed, and 0 if neither or both keys are pressed
+    player_direction.y = int(keys[pygame.K_DOWN] - keys[pygame.K_UP]) 
+    player_rect.center += player_direction * player_speed * dt
+    player_direction = player_direction.normalize() if player_direction else player_direction # normalizes the player direction vector to have a magnitude of 1, but only if the vector is not zero to avoid division by zero errors
+    #print(player_direction)
+
+    recent_keys = pygame.key.get_just_pressed()
+    if recent_keys[pygame.K_SPACE]:
+            print("fire laser") 
             # draw the game elements here
     display_surface.fill('darkgray')
     for pos in star_positions:
@@ -49,11 +67,10 @@ while running:
     # draws the surface onto the display at the specified position    
     display_surface.blit(meteor_surf, meteor_rect)
     display_surface.blit(laser_surf, laser_rect)
-
-
-    # player movement
-    player_rect.center += player_direction  * player_speed # using vector math to move the player in a specific direction
     display_surface.blit(player_surf, player_rect) 
+    
+    #player_rect.center += player_direction  * player_speed * dt # using vector math to move the player in a specific direction
+   
     
     
 
